7070941be44677179dd5eabd06c4293540a73040
Warlav <warlav@gmail.com>
2024-08-16 02:23:34 +0300

    Первое дополнительное задание по циклам.

--- advanced_2.py
+++ advanced_2.py
@@ -1,5 +1,8 @@
 import random
+
+list = []
 i = 0
-while i < 6:
-    print(random.randint(1, 6), random.randint(1, 6))
+while i < 12:
+    list.append(random.randint(1, 6))
     i = i + 1
+print(f'{list[0]} {list[1]}, {list[2]} {list[3]}, {list[4]} {list[5]}, {list[6]} {list[7]}, {list[8]} {list[9]}, {list[10]} {list[11]}')
